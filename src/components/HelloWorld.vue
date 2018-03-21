b7f4358072968c58363b7ee4f74da62fa8f9e9a6
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-03-21 18:04:44 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-form inline>
+      <label class="sr-only" for="inlineFormInputName2">Name</label>
+      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="Jane Doe" />
+      <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+      <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inlineFormInputGroupUsername2" placeholder="Username" />
+      </b-input-group>
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+      <b-button variant="primary">Save</b-button>
+    </b-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
