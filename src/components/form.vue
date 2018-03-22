8cee462ca1724648495f417dae2453b27d39f0ba
sampoultier <samuel.poultier@ynov.com>
2018-03-22 10:36:47 +0100

    test v-for

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,99 +1,104 @@
-<template>
-<v-app>
-<v-form v-model="valid">
+<template> <!--container du template qui va être appelé-->
+<v-app> <!--container de l'application c'est nécessaire d'en avoir un -->
+    <v-form v-model="valid" ref="form" lazy-validation> <!--container du formulaire-->
+        <v-container> 
+            <v-layout row wrap>
+                <v-flex xs12 md6 v-for="logo in logos"> <!-- tant qu'il y a des objets logos on affiche la div avec les valeurs correspondant à l'objet ac-->
+                    <v-card>
+                        <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px"></v-card-media>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        
+        <v-container>
+            <v-layout row wrap>
+                <v-flex lg6 md6 xs12 v-for="input in inputs">
+                    <v-text-field
+                    label="Name"
+                    v-model="name"
+                    :rules="nameRules"
+                    required
+                    >
+                    </v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
 
-   <v-container>
-    <v-layout>
-        <v-flex lg6 md6 xs12>
-            <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px"></v-card-media>
-        </v-flex>
-        <v-flex offset-md-2 lg6 md6 xs12>
-            <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px"></v-card-media>
-        </v-flex>
-    </v-layout>
-    </v-container>
-    <v-container>
-    <v-layout row wrap>
-    <v-flex lg6 md6 xs12>
-    <v-text-field
-                  label="Name"
-                  v-model="name"
-                  :rules="nameRules"
-                  required
-                  ></v-text-field>
-    </v-flex>
-    <v-flex lg6 md6 xs12>
-    <v-text-field
-                  label="E-mail"
-                  v-model="email"
-                  :rules="emailRules"
-                  required
-                  ></v-text-field>
-    </v-flex>
-    </v-layout>
-    </v-container>
-    
-    <v-container>
-        <v-layout row wrap>
-            <v-flex xs12 md6>
-                <v-card>
-                    <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px">
-    </v-card-media>
-                    <v-card-title primary-title class='error white--text'>
-                        <div>
-                            <h3 class="headline mb-0">Produit1</h3>
-                            <div></div>
-    </div>
-    </v-card-title>
-                    <v-card-actions>
-                        <v-btn flat color="orange">Share</v-btn>
-                        <v-btn flat color="orange">Explore</v-btn>
-    </v-card-actions>
-    </v-card>
-    </v-flex>
-            <v-flex xs12 md6>
-                <v-card>
-                    <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px">
-    </v-card-media>
-                    <v-card-title primary-title class='error white--text'>
-                        <div>
-                            <h3 class="headline mb-0">Produit2</h3>
-                            <div></div>
-    </div>
-    </v-card-title>
-                    <v-card-actions>
-                        <v-btn flat color="orange">Share</v-btn>
-                        <v-btn flat color="orange">Explore</v-btn>
-    </v-card-actions>
-    </v-card>
-    </v-flex>
-    </v-layout>
-    </v-container>
-</v-form>
-    </v-app>
+        <v-container>
+            <v-layout row wrap>
+                <v-flex xs12 md6 v-for="card in cards">
+                    <v-card>
+                        <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/desert.jpg" height="200px">
+                        </v-card-media>
+                        <v-card-title primary-title class='error white--text'>
+                            <div>
+                                <h3 class="headline mb-0 text-center">{{ card.title }}</h3>
+                            </div>
+                        </v-card-title>
+                            <v-card-actions>
+                                <v-checkbox v-model="includeFiles" label="i'm interested"></v-checkbox>
+                            </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</v-app>
 
 </template>
 <script>
     export default {
         name: 'form',
-        data () {
-            return {
-                msg: 'Welcome to Your Vue.js App'
-            }
-        }
+        $_veeValidate: {
+            validator: 'new'
+        },
+        data: () => ({
+            logos: [
+                {
+                    path: 'https://vuetifyjs.com/static/doc-images/cards/sunshine.jpg',
+                    title: 'Città'
+                },
+                {
+                    path: 'https://vuetifyjs.com/static/doc-images/cards/sunshine.jpg',
+                    title: 'Compagnie de provence'
+                }
+            ],
+            cards: [
+                {
+                    path: 'https://vuetifyjs.com/static/doc-images/cards/desert.jpg',
+                    title: 'Collection1'
+                },
+                {
+                    path: 'https://vuetifyjs.com/static/doc-images/cards/sunshine.jpg',
+                    title: 'Collection2'
+                }
+            ],
+            inputs: [
+                {
+                    name_label:"Name",
+                    name_model:"name",
+                    name_label:"nameRules"
+                },
+                {
+                    name_label:"E-mail",
+                    name_model:"email",
+                    name_label:"emailRules"
+                }
+            ],
+            includeFiles: false,
+            enabled: false
+        })
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    img.img_header{
-        width:45vh;
-        height: 100%;
-        margin:15px 15px;
+    /*container d'un produit*/
+    div.card{
+        margin:3% 3%; /*top/bottom right/left*/
     }
-    div.img_header{
-        width:100px;
-        height: 100%;
-        margin:15px 15px;
+    div.card img{
+       
     }
 </style>
