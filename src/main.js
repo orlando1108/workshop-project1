e190aa750707621e6346370b01644026c25b54fe
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-03-24 00:46:05 +0100

    fix server

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
