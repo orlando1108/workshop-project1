a277dc4fc2dff0128a7d64af4cdaacc43bdba4ee
sampoultier <samuel.poultier@ynov.com>
2018-03-24 11:38:33 +0100

    bouton + fonctionelle

--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -7,14 +7,14 @@
                 <h1>Nos collections</h1>
             </v-flex>
             <v-layout row wrap>
-                <v-flex xl2 lg3 md4 sm6 xs12 v-for="card in cards">
+                <v-flex xl2 lg3 md4 sm6 xs12 v-for="card in cards" :id="card.title"> <!--la div contenant chaque collection a pour id le nom de sa collection-->
                     <v-card>
                         <v-container class="container_icones">
                             <v-btn fab dark medium color="indigo" class="btn_add" 
-                               v-on:click="add_products">
+                            @click="add_products(card.title)"> <!--quand on clique on appelle ma function qui a pour argument le titre de la collection-->
                                 <v-icon dark>add</v-icon>
                             </v-btn>
-                            <v-btn fab dark medium color="pink" class="btn_cancel" v-on:click="delete_products">
+                            <v-btn fab dark medium color="pink" class="btn_cancel" @click="delete_products(card.title)">
                                 <v-icon dark>favorite</v-icon>
                             </v-btn>
                         </v-container>
@@ -24,7 +24,7 @@
                             
                             </v-card-media>
                        
-                        <v-card-title primary-title :id="card.title" class='white--text'>
+                        <v-card-title primary-title class='white--text'>
                                 <h3 class="headline mb-0">{{ card.title }}</h3>
                         </v-card-title>
                         </router-link>
@@ -92,15 +92,16 @@
         }),
         // Définissez les méthodes de l'objet
         methods: {
-            add_products: function (event) {
-                // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
-                document.querySelector(".btn_add").style.display="none",
-                document.querySelector(".btn_cancel").style.display="block";
+            add_products: function (title) { //ma fonction mettre l'argument recupere ici le titre entre paranthese
+                //on selectionne le bouton add de la div avec pour id la valeur dans la variable titre
+                document.querySelector("div[id='"+title+"'] .btn_add").style.display="none",
+                    document.querySelector("div[id='"+title+"'] .btn_cancel").style.display="block";
+                console.log(title);
             },
-            delete_products: function (event) {
-                // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
-                document.querySelector(".btn_add").style.display="block",
-                document.querySelector(".btn_cancel").style.display="none";
+            delete_products: function (title) {
+                document.querySelector("div[id='"+title+"'] .btn_add").style.display="block",
+                    document.querySelector("div[id='"+title+"'] .btn_cancel").style.display="none";
+                console.log(title);
             }
         }
     }
@@ -134,22 +135,22 @@
     selon le document sur le drive (sharepoint/docs/pantone&policy)
     https://drive.google.com/drive/folders/1up_xhP11L64vZTDStzCjKSOppzEfP-bO
     */
-    div[id="Black & White"]{
+    div[id="Black & White"] div.card__title{ /*selection de la div contenant le titre enfant de la div qui a pour id black & white*/
         background-color:#231F20;
     }
-    div[id="Extra Pur"]{
+    div[id="Extra Pur"] div.card__title{
         background-color:#CFD1CE;
     }
-    div[id="Grooming For Men"]{
+    div[id="Grooming For Men"] div.card__title{
         background-color:#357AA1;
     }
-    div[id="Karite"]{
+    div[id="Karite"] div.card__title{
         background-color:#d9c89e;
     }
-    div[id="Maison Home"]{
+    div[id="Maison Home"] div.card__title{
         background-color:#53C1B6;
     }
-    div[id="Version Originale"]{
+    div[id="Version Originale"] div.card__title{
         background-color:#5B5E5F;
     }
 </style>
